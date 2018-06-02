68b4be9e8fd3b2d8d102c6e0d72fc1c51df18d08
David Stancu <dstancu@nyu.edu>
2018-06-01 22:07:56 -0400

    add basic char vector

---
+++ handlebars/src/main/java/com/github/jknack/handlebars/internal/HbsCharVector.java
@@ -0,0 +1,67 @@
+package com.github.jknack.handlebars.internal;
+
+import java.util.Arrays;
+
+/**
+ * A very basic character vector.
+ *
+ * David Stancu <dstancu@nyu.edu>
+ */
+public final class HbsCharVector {
+  private char[] data;
+  private int size;
+
+  /**
+   * Create a char vector from an existing char[]
+   * @param initial char[]
+   */
+  HbsCharVector(char[] initial) {
+    this.data = initial;
+  }
+
+  /**
+   * Get the backing char[]
+   * @return A char[]
+   */
+  public char[] data() {
+    return this.data;
+  }
+
+  /**
+   * Get number of objects in vector
+   * @return The size
+   */
+  public int getSize() {
+    return this.size;
+  }
+
+  /**
+   * Add char[]
+   * @param vals A char[]
+   */
+  public void addChars(char[] vals) {
+    if ((this.size + vals.length) > this.data.length) grow(vals.length);
+    System.arraycopy(vals, 0, this.data, this.size, vals.length);
+    this.size += vals.length;
+  }
+
+  /**
+   * Add a char
+   * @param val A char
+   */
+  public void addChar(char val) {
+    if (this.size + 1 > this.data.length) grow(1);
+    this.data[this.data.length] = val;
+    this.size += 1;
+  }
+
+  /**
+   * Grow array
+   *
+   * @param sizeOfNew Size of new char[]
+   */
+  private void grow(int sizeOfNew) {
+    sizeOfNew += this.size;
+    this.data = Arrays.copyOf(this.data, sizeOfNew * 2);
+  }
+}
